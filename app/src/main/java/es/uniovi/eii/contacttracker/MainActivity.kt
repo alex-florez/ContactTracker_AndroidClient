957d914664f6f03a28839f1b9e703e76aa897c10
Alex Florez <UO264371@uniovi.es>
2021-02-15 12:31:16 +0100

    Initial commit

---
+++ app/src/main/java/es/uniovi/eii/contacttracker/MainActivity.kt
@@ -0,0 +1,11 @@
+package es.uniovi.eii.contacttracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
