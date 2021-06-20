e513f6f1110e2b923b5c1f89f9d90ae6b7b619a9
Alex Florez <UO264371@uniovi.es>
2021-06-20 13:34:32 +0200

    Añadiendo icono de contactos de riesgo a la opción de menú

--- app/src/test/java/es/uniovi/eii/contacttracker/ExampleUnitTest.kt
+++ app/src/test/java/es/uniovi/eii/contacttracker/ExampleUnitTest.kt
@@ -1,8 +1,12 @@
 package es.uniovi.eii.contacttracker
 
+import android.util.Log
+import es.uniovi.eii.contacttracker.model.Itinerary
+import es.uniovi.eii.contacttracker.util.readFile
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +14,22 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    var lines: List<String> = listOf()
+
+    @Before
+    fun init(){
+        read()
+    }
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        assertEquals("linea1", lines[0])
+    }
+
+    private fun read(){
+        javaClass.classLoader?.getResourceAsStream("prueba.txt")?.bufferedReader().use {
+            if(it != null)
+                lines = it.readLines()
+        }
     }
 }
