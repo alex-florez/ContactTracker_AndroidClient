add12ce26dc523c998c4f5b4c0f044fba12f1f26
Alex Florez <UO264371@uniovi.es>
2021-08-10 13:38:42 +0200

    Añadiendo recurso de selector de colores para el botón de añadir

--- app/src/main/java/es/uniovi/eii/contacttracker/fragments/riskcontacts/RiskContactFragment.kt
+++ app/src/main/java/es/uniovi/eii/contacttracker/fragments/riskcontacts/RiskContactFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getColor
+import androidx.core.content.ContextCompat.getDrawable
 import androidx.core.view.children
 import androidx.core.view.isEmpty
 import androidx.fragment.app.viewModels
