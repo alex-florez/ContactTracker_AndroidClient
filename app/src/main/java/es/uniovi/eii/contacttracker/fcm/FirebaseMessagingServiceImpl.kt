2c3652920169e9bbec4790a20473a547d9cba54d
Alex Florez <UO264371@uniovi.es>
2021-09-09 15:50:16 +0200

    Añadido SDK de Firebase y servicio de Firebase para recibir notificaciones push

---
+++ app/src/main/java/es/uniovi/eii/contacttracker/fcm/FirebaseMessagingServiceImpl.kt
@@ -0,0 +1,9 @@
+package es.uniovi.eii.contacttracker.fcm
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+/**
+ * Servicio para recibir los mensajes de Firebase Cloud Messaging.
+ */
+class FirebaseMessagingServiceImpl : FirebaseMessagingService() {
+}
